3db30690ca1a7b6e897db441ee348c9aec64546e
jachernita <106092183+jachernita@users.noreply.github.com>
2022-05-23 09:26:27 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,15 @@
+$(".first").click(function() {
+    $(".mainImage").css("background-color", "grey");
+});
+
+$(".second").click(function() {
+    $(".mainImage").css("background-color", "cyan");
+});
+
+$(".third").click(function() {
+    $(".mainImage").css("background-color", "lightgreen");
+});
+
+$(".fourth").click(function() {
+    $(".mainImage").css("background-color", "orange");
+});
